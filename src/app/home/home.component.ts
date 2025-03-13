94986a9b15ebd575a8cb84abd2397500afc9ebf2
Milus03 <reitermilan@ktch.hu>
2025-03-13 11:22:41 +0100

    news service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseService } from '../base.service';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,22 @@ import { BaseService } from '../base.service';
 export class HomeComponent {
 
   hatterek: any[] = []
+  nbaNews: any[] = []
 
-  constructor(private base:BaseService){
+  constructor(private base:BaseService, private news:NewsService){
     this.getHatterek()
   }
 
   getHatterek() {
     this.base.getProducts().subscribe((data:any) => {
       this.hatterek = data.hatterek
+    })
+  }
+
+  getNews(){
+    this.news.getNBANews().subscribe((data) => {
+      this.nbaNews = data.articles.slice(0, 20)
+      console.log(this.nbaNews)
     })
   }
 
